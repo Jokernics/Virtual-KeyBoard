5cf7556b8777afaad0cac58e2cbb62248d8a5f6e
egor <spirin.egor13@gmail.com>
2022-05-07 11:17:21 +0300

    feat: add data keys

---
+++ src/bd.js
@@ -0,0 +1,481 @@
+const line1 = {
+  "Backquote": {
+      "RU": {
+          "down": "ё",
+          "Up": "Ё"
+      },
+      "EN": {
+          "down": "`",
+          "Up": "~"
+      }
+  },
+  "Digit1": {
+      "RU": {
+          "down": "1",
+          "Up": "!"
+      },
+      "EN": {
+          "down": "1",
+          "Up": "!"
+      }
+  },
+  "Digit2": {
+      "RU": {
+          "down": "2",
+          "Up": "\""
+      },
+      "EN": {
+          "down": "2",
+          "Up": "@"
+      }
+  },
+  "Digit3": {
+      "RU": {
+          "down": "3",
+          "Up": "№"
+      },
+      "EN": {
+          "down": "3",
+          "Up": "#"
+      }
+  },
+  "Digit4": {
+      "RU": {
+          "down": "4",
+          "Up": ";"
+      },
+      "EN": {
+          "down": "4",
+          "Up": "$"
+      }
+  },
+  "Digit5": {
+      "RU": {
+          "down": "5",
+          "Up": "%"
+      },
+      "EN": {
+          "down": "5",
+          "Up": "%"
+      }
+  },
+  "Digit6": {
+      "RU": {
+          "down": "6",
+          "Up": ":"
+      },
+      "EN": {
+          "down": "6",
+          "Up": "^"
+      }
+  },
+  "Digit7": {
+      "RU": {
+          "down": "7",
+          "Up": "?"
+      },
+      "EN": {
+          "down": "7",
+          "Up": "&"
+      }
+  },
+  "Digit8": {
+      "RU": {
+          "down": "8",
+          "Up": "*"
+      },
+      "EN": {
+          "down": "8",
+          "Up": "*"
+      }
+  },
+  "Digit9": {
+      "RU": {
+          "down": "9",
+          "Up": "("
+      },
+      "EN": {
+          "down": "9",
+          "Up": "("
+      }
+  },
+  "Digit0": {
+      "RU": {
+          "down": "0",
+          "Up": ")"
+      },
+      "EN": {
+          "down": "0",
+          "Up": ")"
+      }
+  },
+  "Minus": {
+      "RU": {
+          "down": "-",
+          "Up": "_"
+      },
+      "EN": {
+          "down": "-",
+          "Up": "_"
+      }
+  },
+  "Equal": {
+      "RU": {
+          "down": "=",
+          "Up": "+"
+      },
+      "EN": {
+          "down": "=",
+          "Up": "+"
+      }
+  }
+}
+
+const line2 = {
+  "KeyQ": {
+      "EN": {
+          "down": "q",
+          "Up": "Q"
+      },
+      "RU": {
+          "down": "й",
+          "Up": "Й"
+      }
+  },
+  "KeyW": {
+      "EN": {
+          "down": "w",
+          "Up": "W"
+      },
+      "RU": {
+          "down": "ц",
+          "Up": "Ц"
+      }
+  },
+  "KeyE": {
+      "EN": {
+          "down": "e",
+          "Up": "E"
+      },
+      "RU": {
+          "down": "у",
+          "Up": "У"
+      }
+  },
+  "KeyR": {
+      "EN": {
+          "down": "r",
+          "Up": "R"
+      },
+      "RU": {
+          "down": "к",
+          "Up": "К"
+      }
+  },
+  "KeyT": {
+      "EN": {
+          "down": "t",
+          "Up": "T"
+      },
+      "RU": {
+          "down": "е",
+          "Up": "Е"
+      }
+  },
+  "KeyY": {
+      "EN": {
+          "down": "y",
+          "Up": "Y"
+      },
+      "RU": {
+          "down": "н",
+          "Up": "Н"
+      }
+  },
+  "KeyU": {
+      "EN": {
+          "down": "u",
+          "Up": "U"
+      },
+      "RU": {
+          "down": "г",
+          "Up": "Г"
+      }
+  },
+  "KeyI": {
+      "EN": {
+          "down": "i",
+          "Up": "I"
+      },
+      "RU": {
+          "down": "ш",
+          "Up": "Ш"
+      }
+  },
+  "KeyO": {
+      "EN": {
+          "down": "o",
+          "Up": "O"
+      },
+      "RU": {
+          "down": "щ",
+          "Up": "Щ"
+      }
+  },
+  "KeyP": {
+      "EN": {
+          "down": "p",
+          "Up": "P"
+      },
+      "RU": {
+          "down": "з",
+          "Up": "З"
+      }
+  },
+  "BracketLeft": {
+      "EN": {
+          "down": "[",
+          "Up": "["
+      },
+      "RU": {
+          "down": "х",
+          "Up": "Х"
+      }
+  },
+  "BracketRight": {
+      "EN": {
+          "down": "]",
+          "Up": "]"
+      },
+      "RU": {
+          "down": "ъ",
+          "Up": "Ъ"
+      }
+  },
+  "Backslash": {
+      "EN": {
+          "down": "\\",
+          "Up": "\\"
+      },
+      "RU": {
+          "down": "\\",
+          "Up": "\\"
+      }
+  }
+}
+
+const line3 = {
+  "KeyA": {
+      "EN": {
+          "down": "a",
+          "Up": "A"
+      },
+      "RU": {
+          "down": "ф",
+          "Up": "Ф"
+      }
+  },
+  "KeyS": {
+      "EN": {
+          "down": "s",
+          "Up": "S"
+      },
+      "RU": {
+          "down": "ы",
+          "Up": "Ы"
+      }
+  },
+  "KeyD": {
+      "EN": {
+          "down": "d",
+          "Up": "D"
+      },
+      "RU": {
+          "down": "в",
+          "Up": "В"
+      }
+  },
+  "KeyF": {
+      "EN": {
+          "down": "f",
+          "Up": "F"
+      },
+      "RU": {
+          "down": "а",
+          "Up": "А"
+      }
+  },
+  "KeyG": {
+      "EN": {
+          "down": "g",
+          "Up": "G"
+      },
+      "RU": {
+          "down": "п",
+          "Up": "П"
+      }
+  },
+  "KeyH": {
+      "EN": {
+          "down": "h",
+          "Up": "H"
+      },
+      "RU": {
+          "down": "р",
+          "Up": "Р"
+      }
+  },
+  "KeyJ": {
+      "EN": {
+          "down": "j",
+          "Up": "J"
+      },
+      "RU": {
+          "down": "о",
+          "Up": "О"
+      }
+  },
+  "KeyK": {
+      "EN": {
+          "down": "k",
+          "Up": "K"
+      },
+      "RU": {
+          "down": "л",
+          "Up": "Л"
+      }
+  },
+  "KeyL": {
+      "EN": {
+          "down": "l",
+          "Up": "L"
+      },
+      "RU": {
+          "down": "д",
+          "Up": "Д"
+      }
+  },
+  "Semicolon": {
+      "EN": {
+          "down": ";",
+          "Up": ";"
+      },
+      "RU": {
+          "down": "ж",
+          "Up": "Ж"
+      }
+  },
+  "Quote": {
+      "EN": {
+          "down": "'",
+          "Up": "'"
+      },
+      "RU": {
+          "down": "э",
+          "Up": "Э"
+      }
+  }
+}
+
+const line4 = {
+  "KeyZ": {
+      "EN": {
+          "down": "z",
+          "Up": "Z"
+      },
+      "RU": {
+          "down": "я",
+          "Up": "Я"
+      }
+  },
+  "KeyX": {
+      "EN": {
+          "down": "x",
+          "Up": "X"
+      },
+      "RU": {
+          "down": "ч",
+          "Up": "Ч"
+      }
+  },
+  "KeyC": {
+      "EN": {
+          "down": "c",
+          "Up": "C"
+      },
+      "RU": {
+          "down": "с",
+          "Up": "С"
+      }
+  },
+  "KeyV": {
+      "EN": {
+          "down": "v",
+          "Up": "V"
+      },
+      "RU": {
+          "down": "м",
+          "Up": "М"
+      }
+  },
+  "KeyB": {
+      "EN": {
+          "down": "b",
+          "Up": "B"
+      },
+      "RU": {
+          "down": "и",
+          "Up": "И"
+      }
+  },
+  "KeyN": {
+      "EN": {
+          "down": "n",
+          "Up": "N"
+      },
+      "RU": {
+          "down": "т",
+          "Up": "Т"
+      }
+  },
+  "KeyM": {
+      "EN": {
+          "down": "m",
+          "Up": "M"
+      },
+      "RU": {
+          "down": "ь",
+          "Up": "Ь"
+      }
+  },
+  "Comma": {
+      "EN": {
+          "down": ",",
+          "Up": ","
+      },
+      "RU": {
+          "down": "б",
+          "Up": "Б"
+      }
+  },
+  "Period": {
+      "EN": {
+          "down": ".",
+          "Up": "."
+      },
+      "RU": {
+          "down": "ю",
+          "Up": "Ю"
+      }
+  },
+  "Slash": {
+      "EN": {
+          "down": "/",
+          "Up": "/"
+      },
+      "RU": {
+          "down": ".",
+          "Up": "."
+      }
+  }
+}
